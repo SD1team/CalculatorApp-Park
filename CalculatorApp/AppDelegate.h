0e848c01c828b562639dfbe2c3de9c7f79f38448
ios <ios@sd1ui-macbook-pro-2.local>
2016-08-30 18:23:11 +0900

    Initial Commit

---
+++ CalculatorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorApp
+//
+//  Created by ios on 2016. 8. 30..
+//  Copyright © 2016년 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorApp
+//
+//  Created by ios on 2016. 8. 30..
+//  Copyright © 2016년 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
